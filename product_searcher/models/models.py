b23b51f19111e1a462e95cfed1d38866787c2211
Gonzalo González Domínguez <meigallo@meigallodixital.com>
2020-02-27 09:24:54 +0100

    Cambiar sistema para usar un usuario compras por tienda

--- product_searcher/models/models.py
+++ product_searcher/models/models.py
@@ -1,6 +1,38 @@
 from dateutil.relativedelta import relativedelta
 from odoo import models, fields, api
 
+class ProductSearcherUsers(models.Model):
+    _name = 'product.searcher.users'
+    _table = "res_users"
+    _auto = False
+    _description = "Product Search Users"
+    _rec_name = "name"
+    _order = 'name, login'
+
+
+    login = fields.Char(required=True)
+    partner_id = fields.Many2one('res.partner',
+        required=True,
+        string='Related Partner')
+    name = fields.Char(related='partner_id.name')
+    default_operating_unit_id = fields.Many2one(
+        'operating.unit', 'Default Operating Unit'
+    )
+
+
+class ProductSearcherOperatingUnit(models.Model):
+    _name = 'product.searcher.operating.unit'
+    _table = "operating_unit"
+    _auto = False
+    _description = "Product Search Operating Unit"
+    _rec_name = "name"
+    _order = 'name'
+
+    name = fields.Char('Name', required=True)
+    code = fields.Char('Code', required=True)
+    active = fields.Boolean('Active', default=True)
+
+
 class ProductSearcher(models.Model):
     _name = 'product.searcher'
     _description = "Product Searcher"
@@ -18,19 +50,17 @@ class ProductSearcher(models.Model):
         ('approved', 'Approved'),
     )
 
-    operating_unit_id = fields.Selection(
-        selection=lambda self: self._compute_operating_unit(),
-        string='Operating Unit',
-        default=lambda self: (
-                str(self.env['res.users'].operating_unit_default_get().id)
-            ),
-        required=True
-        
-    )
+    operating_unit_id = fields.Many2one(
+        'product.searcher.operating.unit',
+        'Operating Unit',
+        default=lambda self: self.env['res.users'
+                                      ].operating_unit_default_get(self._uid),
+        required=True)
     name = fields.Char(required=True)
     client = fields.Char(required=True)
     description = fields.Text()
     phone = fields.Char(required=True)
+    email = fields.Char()
     currency_id = fields.Many2one(
         'res.currency',
         string='Currency'
@@ -44,6 +74,16 @@ class ProductSearcher(models.Model):
         STATES,
         required=True,
         default='pending')
+    applicant_id = fields.Many2one(
+        'product.searcher.users',
+        'Applicant',
+        required=True
+        )
 
-    def _compute_operating_unit(self):
-        return [(str(ou.id), ou.name) for ou in self.env['operating.unit'].sudo().search([('active', '=', True)]).sorted(key=lambda r: r.name)]
+    @api.onchange('operating_unit_id')
+    def _applicants_onchange(self):
+        if self.operating_unit_id:
+            return {'domain': {'applicant_id': [('default_operating_unit_id', '=', self.operating_unit_id.id)]}}
+        return {}
+
+    
